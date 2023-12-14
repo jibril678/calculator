7fa40435c703e9ea25caa53fb5b0bd68663fa3f3
Jibril <jibrilabdi01@gmail.com>
2023-12-14 18:23:23 +0000

    add JS variables

---
+++ script.js
@@ -0,0 +1,17 @@
+// Global Variables
+const allButtons = document.querySelectorAll('buttons')
+const operandButtons = document.querySelectorAll('.operand')
+const operatorButtons = document.querySelectorAll('.operator')
+const decimalButton = document.querySelector('.decimal')
+const allClearButton = document.querySelector('.all-clear')
+const clearButton = document.querySelector('.clear')
+const percentageButton = document.querySelector('.percentage')
+const equalsButton = document.querySelector('#equals')
+const displayOutput = document.querySelector('.display')
+let operandA = []
+let operandB = []
+let operator = []
+
+
+
+
