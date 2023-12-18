a40ea6ad68d8a7d526428aa9abeb1483ea459234
Jibril <jibrilabdi01@gmail.com>
2023-12-18 22:12:28 +0000

    update JS logic

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 // Global Variables
+let operandA = []
+let operandB = []
+let operator = []
+let displayResult = 0
+let currentNumber = operandA
+let equalsClicked = false
 const allButtons = document.querySelectorAll('buttons')
 const operandButtons = document.querySelectorAll('.operand')
 const operatorButtons = document.querySelectorAll('.operator')
@@ -7,37 +13,47 @@ const allClearButton = document.querySelector('.all-clear')
 const clearButton = document.querySelector('.clear')
 const equalsButton = document.querySelector('.equals')
 const displayOutput = document.querySelector('.display')
-let operandA = []
-let operandB = []
-let operator = []
-let currentNumber
-let displayResult
+
+displayOutput.textContent = displayResult
+
 
 // Functions 
-function clearDisplay () {
-    displayOutput.textContent = "0"
-    operandA = []
-    operandB = []
-    operator = []
+const sum = (a, b) => a + b
+const subtract = (a, b) => a - b
+const multiply = (a, b) => a * b
+const divide = (a, b) => a / b
+
+function calculate (a, b, currentOperator) {
+    let result
+    a = Number(operandA.join(''))
+    b = Number(operandB.join(''))
+    currentOperator = operator.join('')
+        if (currentOperator == "+") {
+            result = sum(a, b)
+        } else if (currentOperator == "-") {
+            result = subtract(a, b)
+        } else if (currentOperator == "*") {
+            result = multiply(a, b)
+        } else if (currentOperator == "/") {
+            result = divide(a, b)
+        }
+    return result
 }
 
-function calculate (numberA, numberB, currentOperator) {
-    let result
-    numberA = Number(operandA.join(''), 10)
-    numberB = Number(operandB.join(''), 10)
-    currentOperator = operator.join('')
-    console.log(numberA, currentOperator, numberB)
-    if (currentOperator == "+") {
-        result = numberA + numberB
-    } else if (currentOperator == "-") {
-        result = numberA - numberB
-    } else if (currentOperator == "*") {
-        result = numberA * numberB
-    } else if (currentOperator == "/") {
-        result = numberA / numberB
-    }
+function clearDisplay () {
+    displayOutput.textContent = 0
+    operandA = []
+    operandB = []
+    operator = [operator]
+    currentNumber = operandA
+}
 
-    return result
+function equalsOperation () {
+    displayResult = Number(calculate(operandA, operandB, operator).toFixed(2))
+    clearDisplay()
+    displayOutput.textContent = displayResult
+    operandA = displayResult.toString().split('')
+    currentNumber = operandA
 }
 
 // Event Listeners
@@ -59,60 +75,84 @@ clearButton.addEventListener('click', () => {
             clearDisplay()
         }
     }
-    console.log(operandA)
-    console.log(operator)
-    console.log(operandB)
 })
 
 operandButtons.forEach(element => {
     element.addEventListener('click', () => {
-        if (operator.length === 0) {
-            currentNumber = operandA
+        if (equalsClicked) {
+            clearDisplay()
+            equalsClicked = false
+        }
+        console.log('Number A:', operandA)
+        console.log('Operator:', operator)
+        console.log('Number B:', operandB)
+        console.log('Current Number:', currentNumber)
+        if (currentNumber === operandA) {
             operandA.push(element.value)
-            if (displayOutput.textContent === "0") {
-                displayOutput.innerHTML = ''
-            }
             displayOutput.textContent = operandA.join('')
-        } else if (operator.length > 0) {
-            console.log('operator clicked')
-            currentNumber = operandB
+        } else if (currentNumber === operandB) {
             operandB.push(element.value)
-            if (displayOutput.textContent === "0") {
-                displayOutput.innerHTML = ''
-            }
             displayOutput.textContent = operandB.join('')
         }
-        console.log(operandA)
-        console.log(operator)
-        console.log(operandB)
+        console.log('END Number A:', operandA)
+        console.log('Operator:', operator)
+        console.log('END Number B:', operandB)
     })
 });
 
 operatorButtons.forEach(element => {
     element.addEventListener('click', () => {
-            if(operator.length > 0) {
-                operator[0] = element.value
-            } else {
-                operator.push(element.value)
-            }
-            console.log(operandA)
-            console.log(operator)
-            console.log(operandB)
+        equalsClicked = false
+        if (operandA.length > 0 && operandB.length > 0) {
+            equalsOperation() 
+        }
+        currentNumber = operandB
+        if(operator.length > 0) {
+            operator[0] = element.value} 
+        else {
+            operator.push(element.value)}
+            console.log('END Operator A:', operandA)
+            console.log('Operator:', operator)
+            console.log('END Operator B:', operandB)
     })
 })
 
 equalsButton.addEventListener('click', () => {
-    displayResult = calculate(operandA, operandB, operator).toFixed(10)
-    displayResult = parseFloat(displayResult)
-    displayOutput.textContent = displayResult
-})
+    equalsOperation()
+    equalsClicked = true
+    console.log('A:', operandA)
+    console.log('Operator:', operator)
+    console.log('B:', operandB)
+}
+)
+
+decimalButton.addEventListener('click', () => {
+    if(currentNumber === operandA) {
+        if (!operandA.includes('.')) {
+            if (operandA.length < 1) {
+            operandA.push('0', '.')
+            displayOutput.textContent = operandA.join('')
+            } else {
+                operandA.push('.')
+                displayOutput.textContent = operandA.join('')
+                }
+            }
+        }
+        else if(currentNumber === operandB) {
+            if (!operandB.includes('.')) {
+                if (operandB.length < 1) {
+                operandB.push('0', '.')
+                displayOutput.textContent = operandB.join('')
+                } else {
+                    operandB.push('.')
+                    displayOutput.textContent = operandB.join('')
+                    }  
+                }
+            }
+    }      
+)
+
 
 // Add NumberA to top of display 
-// highlight operator button when selected
-// Fix decimal being clicked without a 0 in front
-// Add separate functions for Sum, Subtract, Multiply, Divide
-// allow multiple calculations 
-// See if you can add commas 
+// Button selection styling
 // Do final styling
-
-
